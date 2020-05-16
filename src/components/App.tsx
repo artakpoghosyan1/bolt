9b4bca96c07f59e4fc3f6cb5109b58812c551354
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-17 02:10:06 +0400

    add basename

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import {css} from 'emotion'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {LoginComponent} from "./LoginComponent";
 import {Container} from "react-bootstrap";
-import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {IState} from "../store/IState";
 import {IAction, reducer} from "../store/reducer";
 import {MenuComponent} from "./MenuComponent";
@@ -38,7 +38,7 @@ const App: React.FunctionComponent = () => {
     return (
         <StateContext.Provider value={{state, dispatch: dispatch!}}>
             <Container className={containerClass}>
-                <Router>
+                <Router basename={process.env.PUBLIC_URL}>
                     <HeaderComponent/>
 
                     <Switch>
